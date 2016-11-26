4ce5d7f84e5d1b0ba8eb9ce3b1ff713d5d491721
wearegodz <leoncioceiestia@yahoo.com.ph>
2016-11-27 00:20:27 +0800

    Update SpellDatabase.cs

--- GosuMechanics_Yasuo/GosuMechanics_Yasuo/SpellDatabase.cs
+++ GosuMechanics_Yasuo/GosuMechanics_Yasuo/SpellDatabase.cs
@@ -1544,32 +1544,6 @@ namespace GosuMechanics_Yasuo
                     MissileSpellName = "JinxWMissile",
                     CanBeRemoved = true,
                     CollisionObjects =
-                        new[]
-                        { CollisionObjectTypes.Minion, CollisionObjectTypes.YasuoWall},
-                });
-
-
-            #region Jinx
-
-            //TODO: Detect the animation from fow instead of the missile.
-            Spells.Add(
-                new SpellData
-                {
-                    ChampionName = "Jinx",
-                    SpellName = "JinxW",
-                    Slot = SpellSlot.W,
-                    Type = SkillShotType.SkillshotMissileLine,
-                    Delay = 600,
-                    Range = 1500,
-                    Radius = 60,
-                    MissileSpeed = 3300,
-                    FixedRange = true,
-                    AddHitbox = true,
-                    DangerValue = 3,
-                    IsDangerous = true,
-                    MissileSpellName = "JinxWMissile",
-                    CanBeRemoved = true,
-                    CollisionObjects =
                         new[] { CollisionObjectTypes.Champion, CollisionObjectTypes.Minion, CollisionObjectTypes.YasuoWall },
                 });
             //TODO: Add Chompers
